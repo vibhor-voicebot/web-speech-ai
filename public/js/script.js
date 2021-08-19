fd058100c51cee764e40ae778215e338683da357
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2021-08-19 15:53:28 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //const socket = io();
-const socket = io({transports: [“websocket”], upgrade: false});
+const socket = io({transports: ["websocket"], upgrade: false});
 
 const outputYou = document.querySelector('.output-you');
 const outputBot = document.querySelector('.output-bot');
